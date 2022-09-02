39e43b1e7022907d1bb25a940d98c4e72bdb30b8
danielkusumap <97284710+danielkusumap@users.noreply.github.com>
2022-09-02 13:32:37 +0700

    Add files via upload

--- songs_api.py
+++ songs_api.py
@@ -1,16 +1,9 @@
 from flask import Flask, render_template, request, session, redirect, url_for, flash, jsonify
 import pandas as pd
-from add_yt_music_link import find_link_and_combine
-import threading
 
 
 app = Flask(__name__)
 data = pd.read_csv("data/songsDataset.csv")
-
-@app.route("/updatedata")
-def updateData():
-    threading.Thread(target=find_link_and_combine).start()
-    return "updating data ..."
 
 @app.route("/")
 def index():
@@ -37,17 +30,7 @@ def api():
             title_year = title_year.loc[data["year_release"] >= int(year)]
         elif yearGTE.upper() == "FALSE":
             title_year = title_year.loc[data["year_release"] == int(year)]
-        # title_year = title_year.loc[data["year_release"] == int(year)]
-        # if int(year) > 0:
-        #     title_year = title_year.loc[data["year_release"] == int(year)]
-        # if int(yearGT) > 0:
-        #     title_year = title_year.loc[data["year_release"] >= int(yearGT)]
     except:
-        # return jsonify(
-        #     artist = artist,
-        #     message = "Bad URI",
-        #     output = "Error"
-        # )
         pass
     finally:
         output = [{"": i,"title": title_year["title"][i], "year_release": str(title_year["year_release"][i]), "ytMusic_URL": str(title_year["ytMusic_URL"][i])} for i in title_year.index]
@@ -73,9 +56,3 @@ def api():
 
 if __name__ == "__main__":
     app.run(threaded=True)
-
-# data = pd.read_csv("data/songsDataset.csv")
-# a = "Tracy Chapman"
-# x = data.loc[data["artists"].str.upper() == a.upper()]
-# x = x.loc[data["year_release"] == 1995]
-# print(x)
